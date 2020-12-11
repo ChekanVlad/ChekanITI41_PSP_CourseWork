b0e149b7964160613bf12c3a944318106d656db6
Чекан Влад <vlad.gnarls@yandex.ru>
2020-12-11 15:56:10 +0300

    (Udp)

--- GameServer/GameServer/MainWindow.xaml.cs
+++ GameServer/GameServer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Udp;
 
 namespace GameServer
 {
@@ -20,9 +21,51 @@ namespace GameServer
     /// </summary>
     public partial class MainWindow : Window
     {
+        Action<string> action;
+        Server server;
+        List<string> consoleText;
         public MainWindow()
         {
             InitializeComponent();
+            action = ShowMessage;
+            consoleText = new List<string>();
+            string Host = System.Net.Dns.GetHostName();
+            ServerIPLabel.Content = "Use " + System.Net.Dns.GetHostByName(Host).AddressList[1].ToString() + " to connect to the server.";
+        }
+
+        private void RunServerButton_Click(object sender, RoutedEventArgs e)
+        {
+            server = new Server(5555);
+            server.Notify += NotifyDispatcher;
+            server.RunServer();
+            RunServerButton.IsEnabled = false;
+            ConsoleTextBlock.Text = "Server launched.";
+        }
+
+        private void NotifyDispatcher(string message)
+        {
+            Dispatcher.Invoke(action, message);
+        }
+
+        private void ShowMessage(string message)
+        {
+            if (consoleText.Count > 10)
+                consoleText.RemoveAt(0);
+            consoleText.Add(DateTime.Now.ToString() + ": " + message + "\n");
+            StringBuilder builder = new StringBuilder();
+            foreach (string str in consoleText)
+            {
+                builder.Append(str);
+            }
+            ConsoleTextBlock.Text = builder.ToString();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (server != null)
+            {
+                server.Close();
+            }
         }
     }
 }
